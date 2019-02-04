7e691f4f3f2f315025e80bfd29b0b9312c3cbd50
Michael Griffiths <2320395g@student.gla.ac.uk>
2019-02-04 22:54:10 +0000

    TWD 5.3 - Modifications made to models.py, no other changes.

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.Charfield(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Page(models.model):
+    category = models.ForeignKey(Category)
+    title = models.CharFiels(max_length=120)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self title
+    
