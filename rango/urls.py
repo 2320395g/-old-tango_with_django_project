efb4b391edc0a1e6aa6ece27c504aadad508d502
Michael Griffiths <2320395g@student.gla.ac.uk>
2019-02-08 15:22:14 +0000

    9.7 - Moving onto 9.8, working.

--- rango/urls.py
+++ rango/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
     
     url(r'^register/$',
         views.register, name='register'),
+
+    url(r'^login/$', views.user_login, name='login'),
     ]
 
--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,9 @@ from rango.models import Page
 from rango.forms import CategoryForm
 from rango.forms import PageForm
 from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 
 
 def index (request):
@@ -121,3 +124,27 @@ def register(request):
         'registered': registered})
 
 
+def user_login(request):
+
+    if request.method =='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+
+    else:
+        return render(request, 'rango/login.html', {})
+    
+            
+    
