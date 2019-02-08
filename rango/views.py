9169f475d29953f966aec7be760218da338f8ed0
Michael Griffiths <2320395g@student.gla.ac.uk>
2019-02-08 16:24:05 +0000

    10.5 - Fully completed, beginning 10.6..

--- rango/views.py
+++ rango/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
-
+from datetime import datetime
 
 def index (request):
     '''construct a dictionary to pass to the template engine as its cotext.
@@ -20,17 +20,21 @@ def index (request):
        shortcut function to make our lives easier. Note that the first
        parameter is the template we wish to use.'''
 
-    context_dict = {}
     
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict['categories'] = category_list
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories' : category_list, 'pages':page_list}
 
-    page_list = Page.objects.order_by('views')[:5]
-    context_dict['pages'] = page_list
+    response = render(request, 'rango/index.html', context_dict)
+
+    visitor_cookie_handler(request, response)
     
-    return render(request, 'rango/index.html', context=context_dict)
+    return response
 
 def about (request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     return render(request, 'rango/about.html')
 
 def show_category(request, category_name_slug):
@@ -138,7 +142,7 @@ def user_login(request):
                 login(request, user)
                 return HttpResponseRedirect(reverse('index'))
             else:
-                return HttpResponse("Your Rango account is disabled.")
+                return HttpResponseRedirect("Your Rango account is disabled.")
 
         else:
             print("Invalid login details: {0}, {1}".format(username, password))
@@ -155,3 +159,20 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+
+def visitor_cookie_handler(request, response):
+
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()) )
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        "%Y-%m-%d %H:%M:%S")
+  
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+  
+    response.set_cookie('visits', visits)
